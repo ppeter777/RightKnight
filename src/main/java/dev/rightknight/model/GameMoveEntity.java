b41c4e980cf19225c13a695e97fc6945b7df056d
Peter Pesotskiy <ppeter777@yandex.ru>
2026-05-29 22:58:16 +0300

    Comment GameMoveEntity.java

--- src/main/java/dev/rightknight/model/GameMoveEntity.java
+++ src/main/java/dev/rightknight/model/GameMoveEntity.java
@@ -1,37 +1,37 @@
-package dev.rightknight.model;
-
-import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
-
-@Entity
-@Table(name = "game_moves")
-@Getter
-@Setter
-public class GameMoveEntity {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    @ManyToOne(optional = false)
-    @JoinColumn(name = "game_id")
-    private GameEntity game;
-
-    private int ply;          // 1, 2, 3...
-    private int moveNumber;   // 1, 1, 2, 2...
-    private boolean whiteMove;
-
-    private String san;       // e4, Nf3, O-O
-    private String uci;       // e2e4
-
-    @Column(columnDefinition = "TEXT")
-    private String fenBefore;
-
-    @Column(columnDefinition = "TEXT")
-    private String fenAfter;
-
-    private Long clockBeforeMs;
-    private Long clockAfterMs;
-    private Long moveTimeMs;
-}
+//package dev.rightknight.model;
+//
+//import jakarta.persistence.*;
+//import lombok.Getter;
+//import lombok.Setter;
+//
+//@Entity
+//@Table(name = "game_moves")
+//@Getter
+//@Setter
+//public class GameMoveEntity {
+//
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    private Long id;
+//
+//    @ManyToOne(optional = false)
+//    @JoinColumn(name = "game_id")
+//    private GameEntity game;
+//
+//    private int ply;          // 1, 2, 3...
+//    private int moveNumber;   // 1, 1, 2, 2...
+//    private boolean whiteMove;
+//
+//    private String san;       // e4, Nf3, O-O
+//    private String uci;       // e2e4
+//
+//    @Column(columnDefinition = "TEXT")
+//    private String fenBefore;
+//
+//    @Column(columnDefinition = "TEXT")
+//    private String fenAfter;
+//
+//    private Long clockBeforeMs;
+//    private Long clockAfterMs;
+//    private Long moveTimeMs;
+//}
